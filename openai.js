19811f96d48377f9ebb80e0463588912f41ddd29
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2022-05-19 08:24:14 +0100

    dialog for code

--- openai.js
+++ openai.js
@@ -27,7 +27,7 @@ window.vincentOpenAI = (function() {
   },
 
   changeApiCode = () => {
-    const apiCode = prompt('Enter API code');
+    const apiCode = prompt('Enter API code', sessionStorage.getItem('vincent-openai__query'));
     sessionStorage.setItem('vincent-openai__key', code);
     return apiCode;
   },
